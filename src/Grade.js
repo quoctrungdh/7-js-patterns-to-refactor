cf6dc6799236a8bc74ec854012da8efa0ba2deac
quoctrungdh <quoctrungdh@gmail.com>
2018-04-01 15:45:57 +0700

    implement Grade object

--- src/Grade.js
+++ src/Grade.js
@@ -1,11 +1,41 @@
 function Grade(percentage) {
   this.percentage = percentage;
+  this.grade = this.grade(percentage);
 }
 
 Grade.prototype = Object.assign({}, Grade.prototype, {
-  getPercentage() {
-    return this.percentage;
+  grade(percentage) {
+    return this.grades.find(grade => percentage >= grade.minimumPercentage);
   },
+
+  isPassing() {
+    return this.grade.passing;
+  },
+
+  letterGrade() {
+    return this.grade.letter;
+  },
+
+  passingGradeLetters() {
+    return this.grades.reduce((accu, grade) => {
+      if (grade.passing) {
+        return accu.concat(grade.letter);
+      }
+      return accu;
+    }, []);
+  },
+
+  isBetterThan(grade) {
+    return this.percentage > grade.percentage;
+  },
+
+  grades: [
+    { letter: 'A', minimumPercentage: 0.9, passing: true },
+    { letter: 'B', minimumPercentage: 0.8, passing: true },
+    { letter: 'C', minimumPercentage: 0.7, passing: true },
+    { letter: 'D', minimumPercentage: 0.6, passing: true },
+    { letter: 'F', minimumPercentage: 0, passing: false },
+  ],
 });
 
 module.exports = Grade;
--- src/Grade.test.js
+++ src/Grade.test.js
@@ -1,10 +1,49 @@
 const Grade = require('./Grade');
 
-describe('Grade functions', () => {
-  it('should return percentage', () => {
-    const p = 0.5;
-    const g = new Grade(p);
+describe('Grade', () => {
+  describe('#passing', () => {
+    it('should pass for 0.7', () => {
+      const grade = new Grade(0.7);
+      expect(grade.isPassing()).toBe(true);
+    });
 
-    expect(g.getPercentage()).toBe(p);
+    it('should NOT pass for anything below 0.6', () => {
+      const grade = new Grade(0.4);
+      expect(grade.isPassing).not.toBe(true);
+    });
+  });
+
+  describe('#letterGrade', () => {
+    it('should return A for 100 percent', () => {
+      const grade = new Grade(1);
+      expect(grade.letterGrade()).toBe('A');
+    });
+
+    it('should return correct letter B for 0.8', () => {
+      const grade = new Grade(0.8);
+      expect(grade.letterGrade()).toBe('B');
+    });
+
+    it('should return F for anything lower than 0,6', () => {
+      const grade = new Grade(0.59);
+      expect(grade.letterGrade()).toBe('F');
+    });
+  });
+
+  describe('#passingLetters', () => {
+    it('should return all passing letters', () => {
+      const grade = new Grade(0);
+      const passingGradeLetters = ['A', 'B', 'C', 'D'];
+      expect(grade.passingGradeLetters()).toEqual(passingGradeLetters);
+    });
+  });
+
+  describe('#isBetterThan', () => {
+    it('should return true if grade is better than comparison grade', () => {
+      const grade1 = new Grade(0.5);
+      const grade2 = new Grade(0.6);
+
+      expect(grade1.isBetterThan(grade2)).toBe(false);
+    });
   });
 });
